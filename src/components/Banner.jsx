6f26ba98ba109059b5b9f1d5ef0981fffc1abfd1
Jeason Campos <camposjeason@gmail.com>
2022-11-14 17:44:19 -0400

    0.6

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,3 @@
-import { Grid, Text } from '@nextui-org/react'
 import { motion } from 'framer-motion'
 
 const Banner2 = () => {
@@ -11,18 +10,18 @@ const Banner2 = () => {
   }
   return (
     <>
-      <Grid.Container className='grid place-content-center bg-orange-600 h-screen w-full'>
+      <div className='grid place-content-center bg-orange-600 h-screen w-full select-none'>
         <motion.div onTap={toggle} className='portrait:h-[45vh] portrait:w-[45vh] landscape:h-[70vh] landscape:w-[70vh] landscape:md:h-[65vh] landscape:md:w-[65vh] bg-slate-50 cursor-pointer relative mb-14 md:mb-0 text-center select-none rounded-[12%]'
           whileTap={{
             scale: [1.01, 1.1],
             rotate: [0, -360],
             borderRadius: ['12%', '50%'],
-            transition: { duration: 0.6 }
+            transition: { duration: 1.2 }
           }}
         >
-          <Text>Hola </Text>
+          <span> Hola </span>
         </motion.div>
-      </Grid.Container>
+      </div>
 
     </>
   )
